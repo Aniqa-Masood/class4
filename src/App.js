e11d581d5f98f3a464e93e7c67bd9d79c9198098
Aniqa-Masood <aniqamasood111@gmail.com>
2020-07-08 02:32:40 +0500

    learning hooks, botton handling,ternary operator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  // creating state
+  // We can create many variables in a function
+  let [count , setCount] = useState(1);
+  let [isMorning, setMorning] = useState(true)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // ?: is a ternary operator
+    // `` backtick is used to concatenate, $ is used to append ,here we are appending nightDark class with other
+    
+    //<div className={isMorning ? 'dayLight' : 'nightDark'}>
+    <div className={`nightDark  ${isMorning ? 'dayLight' : ''}`}>
+
+      <h1>Time is : {isMorning ? 'Morning' : 'Night'}</h1>
+      <h1>The value of counter is : {count} </h1>
+      <button onClick={() => setCount(count + 1)}>Update Counter</button>
+      <button onClick={()=> setMorning(!isMorning)}>Update time</button>
     </div>
   );
+
+
 }
 
 export default App;
